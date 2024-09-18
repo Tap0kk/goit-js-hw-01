0e628117661bc15aadbd208e9a1b3e3bf8bd288f
Deman <deman2805@gmail.com>
2024-09-18 15:22:13 +0300

    +

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+`use strict`
 function makeTransaction(quantity, pricePerdroid) {
     return `You ordered ${quantity} droids worth ${quantity * pricePerdroid} credits!`
 }
@@ -5,3 +6,5 @@ function makeTransaction(quantity, pricePerdroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+ console.log(`----------------------`)
